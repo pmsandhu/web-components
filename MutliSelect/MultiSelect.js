85f6da3a68b7b421eed29ef74fd38dffa2af727b
Patrice Sandhu <pmsandhu@ucla.edu>
2017-12-25 22:55:12 -0800

    refactor multiselect and rename items

--- MutliSelect/MultiSelect.js
+++ MutliSelect/MultiSelect.js
@@ -1,138 +1,83 @@
-const ownerDocument = document.currentScript.ownerDocument
-const template = ownerDocument.querySelector('#multiselectTemplate')
+const template = document.currentScript.ownerDocument.querySelector('#multiselectTemplate')
 
 class MultiSelect extends HTMLElement {
+  constructor() {
+    super()
+    this.root = this.createShadowRoot()
+    this.root.appendChild(template.content.cloneNode(true))
+
+    this.focusedIdx = 0
+    this.isOpen = false
+
+    this.parentComponent = this.root.querySelector('.multiselect')
+    this.tagContainer = this.root.querySelector('.tag-container')
+    this.tagContainerPlaceholder = this.root.querySelector('.tag-container-placeholder')
+    this.selectDropdown = this.root.querySelector('.select-dropdown')
+    this.selectOptions = this.root.querySelector('.select-options')
+    this.li = this.querySelectorAll('li')
+  }
+
   connectedCallback() {
-    this.init()
-    this.render()
-  }
-  init() {
-    const root = this.createShadowRoot()
-    root.appendChild(template.content.cloneNode(true))
-    this.root = root
-
-    this.options = { placeholder: this.getAttribute('placeholder') || 'Select' }
-    this.control = this.root.querySelector('.multiselect')
-    this.field = this.root.querySelector('.multiselect-field')
-    this.popup = this.root.querySelector('.multiselect-popup')
-    this.list = this.root.querySelector('.multiselect-list')
+    this.attachAttributes()
+    this.attachEventHandlers()
+    this.updateVisibleOptions()
+    this.toggleTagContainerPlaceholder()
   }
 
-  render() {
-    this.attachHandlers()
-    this.refreshItems()
-    this.createPlaceholder()
+  attachAttributes() {
+    this.tagContainerPlaceholder.innerText = this.getAttribute('placeholder') || 'Select'
+    this.li.forEach((val, i) => val.setAttribute('tabindex', i))
   }
 
-  attachHandlers() {
-    this.field.addEventListener('click', this.fieldClickHandler.bind(this))
-    this.list.addEventListener('click', this.listClickHandler.bind(this))
-    this.control.addEventListener('keydown', this.keyDownHandler.bind(this))
+  attachEventHandlers() {
+    this.parentComponent. addEventListener('keydown', this.keyDownHandler.bind(this))
+    this.selectOptions.addEventListener('click', e => this.selectOptionElement(e.target))
+    this.tagContainer.addEventListener('click', e => this.isOpen ? this.close() : this.open())
+    this.li.forEach(val => val.addEventListener('mouseenter', this.hoverHandler.bind(this)))
   }
 
-  fieldClickHandler() { this.isOpened ? this.close() : this.open() }
+  toggleTagContainerPlaceholder() {
+    this.tagContainerPlaceholder.style.display = this.querySelectorAll('li[selected]').length ? 'none' : 'block'
+  }
+
+  updateVisibleOptions() {
+    this.li.forEach(val => val.style.display = val.hasAttribute('selected') ? 'none' : 'block')
+    this.focusedIdx = 0
+    this.setFocusedOptionElement()
+  }
 
   open() {
-    this.togglePopup(true)
-    this.refreshFocusedItem()
+    this.isOpen = true
+    this.selectDropdown.style.display = 'block'
+    this.setFocusedOptionElement()
   }
 
   close() {
-    this.togglePopup(false)
-    this.field.focus()
+    this.isOpen = false
+    this.selectDropdown.style.display = 'none'
+    this.tagContainer.focus()
   }
 
-  togglePopup(show) {
-    this.isOpened = show
-    this.popup.style.display = show ? 'block' : 'none'
-    this.control.setAttribute('aria-expanded', show)
-  }
-
-  keyDownHandler(e) {
-    switch(e.which) {
-      case 8: //backspace
-        const tags = this.root.querySelectorAll('.multiselect-tag')
-        if (!tags.length) return
-        const lastTag = tags[tags.length - 1]
-        for (const i of this.itemElements())
-          if (i.textContent === lastTag.textContent) {
-            i.removeAttribute('selected')
-            i.setAttribute('aria-selected', false)
-            lastTag.remove()
-            this.refreshItems()
-            this.createPlaceholder()
-          }
-        break
-      case 27: // escape
-        return this.close()
-      case 38: // down arrow
-        this.focusedIdx = this.focusedIdx == 0 ? this.itemElementsDD().length - 1 : --this.focusedIdx
-        return this.refreshFocusedItem()
-      case 40: // up arrow
-        this.focusedIdx = this.focusedIdx == this.itemElementsDD().length - 1 ? 0 : ++this.focusedIdx
-        return this.refreshFocusedItem()
-      case 13: // enter
-        if (this.isOpened)
-          return this.selectItem(this.itemElementsDD()[this.focusedIdx])
-      default:
-        return e.preventDefault()
+  selectOptionElement(optionElement) {
+    if (!optionElement.hasAttribute('selected')) {
+      optionElement.setAttribute('selected', 'selected')
+      this.tagContainer.appendChild(this.createTag(optionElement))
     }
+    this.updateVisibleOptions()
+    this.toggleTagContainerPlaceholder()
   }
 
-  itemElements() { return this.querySelectorAll('li') }
-  itemElementsDD() { return this.querySelectorAll('li[style="display: block;"]') }
-  refreshFocusedItem() { this.itemElementsDD()[this.focusedIdx].focus() }
-
-  listClickHandler(e) {
-    let item = e.target
-    while (item && item.tagName !== 'LI') item = item.parentNode
-    this.selectItem(item)
-  }
-
-  selectItem(item) {
-    this.removePlaceHolder()
-    if (!item.hasAttribute('selected')) {
-      item.setAttribute('selected', 'selected')
-      item.setAttribute('aria-selected', true)
-      this.fireChangeEvent()
-      this.field.appendChild(this.createTag(item))
-    }
-    this.refreshItems()
-    this.close()
-  }
-
-  refreshItems() {
-    for (const i of  this.itemElements()) {
-      i.setAttribute('role', 'option')
-      i.setAttribute('aria-selected', i.hasAttribute('selected'))
-      i.hasAttribute('selected')
-        ? i.style.display = 'none'
-        : i.style.display = 'block'
-      i.setAttribute('tabindex', -1)
-    }
-    this.focusedIdx = 0
-  }
-
-  removeTag(tag, item, e) {
-    item.removeAttribute('selected')
-    item.setAttribute('aria-selected', false)
-    for (const i of this.field.children) if (item.textContent == i.textContent) i.remove()
-    this.fireChangeEvent()
-    this.createPlaceholder()
-    this.refreshItems()
-    e.stopPropagation()
-
-  }
-  createTag(item) {
+  createTag(option) {
     const tag = document.createElement('div')
-    tag.className = 'multiselect-tag'
+    tag.className = 'tag'
+
     const content = document.createElement('div')
-    content.className = 'multiselect-tag-text'
-    content.textContent = item.textContent
+    content.className = 'tag-text'
+    content.textContent = option.textContent
 
     const removeButton = document.createElement('div')
-    removeButton.className = 'multiselect-tag-remove-button'
-    removeButton.addEventListener('click', this.removeTag.bind(this, tag, item))
+    removeButton.className = 'tag-remove-button'
+    removeButton.addEventListener('click', this.removeTag.bind(this, tag, option))
 
     tag.appendChild(content)
     tag.appendChild(removeButton)
@@ -140,33 +85,61 @@ class MultiSelect extends HTMLElement {
     return tag
   }
 
-  createPlaceholder() {
-    if (this.querySelectorAll('li[selected]').length) return
-    const placeholder = document.createElement('div')
-    placeholder.className = 'multiselect-field-placeholder'
-    placeholder.textContent = this.options.placeholder
-    this.field.appendChild(placeholder)
-  }
-  removePlaceHolder() {
-    const placeholder = this.root.querySelector('.multiselect-field-placeholder')
-    if (placeholder) placeholder.remove()
+  removeTag(tag, option, e) {
+    option.removeAttribute('selected')
+    tag.remove()
+    this.updateVisibleOptions()
+    this.toggleTagContainerPlaceholder()
+    e.stopPropagation()
   }
 
-  selectedItems() {
-    const result = []
-    const selectedItems = this.querySelectorAll('li[selected]')
-    for (const i of selectedItems)
-      result.push(i.hasAttribute('value') ? i.getAttribute('value') : i.textContent)
-    return result
+  keyDownHandler(e) {
+    switch(e.which) {
+      // BACKSPACE
+      case 8:
+        const tags = this.root.querySelectorAll('.tag')
+        if (!tags.length) return
+        const lastTag = tags[tags.length - 1]
+        return this.removeTag(lastTag, this.findElement('li', 'textContent', lastTag.textContent), e)
+      // ESCAPE
+      case 27:
+        return this.close()
+      // DOWN_ARROW
+      case 38:
+        this.focusedIdx = this.focusedIdx == 0 ? this.getVisibleOptions().length - 1 : --this.focusedIdx
+        return this.setFocusedOptionElement()
+      // UP_ARROW
+      case 40:
+        this.focusedIdx = this.focusedIdx == this.getVisibleOptions().length - 1 ? 0 : ++this.focusedIdx
+        return this.setFocusedOptionElement()
+      // ENTER
+      case 13:
+        if (this.isOpen) return this.selectOptionElement(this.getVisibleOptions()[this.focusedIdx])
+      default:
+        return e.preventDefault()
+    }
   }
-  fireChangeEvent() {
-    const e = new CustomEvent('change')
-    this.dispatchEvent(e)
+
+  hoverHandler(e) {
+    e.target.focus()
+    let i = 0
+    const visibleOptions = this.getVisibleOptions()
+    while (visibleOptions[i] != e.target) ++i
+    this.focusedIdx = i
   }
-  attributeChangedCallback(optionName, oldValue, newValue) {
-    console.log('attributeChangedCallback')
-    this.options[optionName] = newValue
-    this.refreshField()
+
+  findElement(tagName, attribute, match) {
+    for (const i of this.querySelectorAll(tagName))
+      if (i[attribute] == match) return i
+  }
+
+  getVisibleOptions() {
+    return this.querySelectorAll('li[style="display: block;"]')
+  }
+
+  setFocusedOptionElement() {
+    const visibleOptions = this.getVisibleOptions()
+    if (visibleOptions.length) visibleOptions[this.focusedIdx].focus()
   }
 }
 

f022294515132435f5518e86be3e1c497209ce2d
Patrice Sandhu <pmsandhu@ucla.edu>
2018-01-05 02:45:24 -0800

    add more key code values to keycode object

--- utils.js
+++ utils.js
@@ -1,12 +1,14 @@
 const KEYCODE = {
+  BACKSPACE: 8,
+  TAB: 9,
+  ENTER: 13,
+  ESCAPE: 27,
+  END: 35,
+  HOME: 36,
+  UP: 38,
   DOWN: 40,
   LEFT: 37,
   RIGHT: 39,
-  UP: 38,
-  HOME: 36,
-  END: 35,
-  TAB: 9,
-  ENTER: 13
 }
 
 function findElement(collection, attribute = 'selected') {

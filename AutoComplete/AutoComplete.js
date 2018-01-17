a4cf2ef7c3f812720b8779a4f6c4c07c26b2727f
Patrice Sandhu <pmsandhu@ucla.edu>
2018-01-17 03:01:43 -0800

    implemented auto complete search componenet

--- AutoComplete/AutoComplete.js
+++ AutoComplete/AutoComplete.js
@@ -1,301 +1,244 @@
-const autoCompleteTemplate = document.currentScript.ownerDocument.querySelector('#autoCompleteTemplate')
-
-// class AutoComplete extends HTMLElement {
-//   constructor() {
-//     super()
-//     this.attachShadow({ mode: 'open' })
-//     this.shadowRoot.appendChild(autoCompleteTemplate.content.cloneNode(true))
-//
-//     this.focusIndex = 0
-//     this.isOpen = false
-//
-//     this.input = this.shadowRoot.querySelector('input')
-//     this.autoComplete = this.shadowRoot.querySelector('.auto-complete')
-//     this.bar = this.shadowRoot.querySelector('.bar')
-//     this.dropdown = this.shadowRoot.querySelector('.dropdown')
-//     this.options = this.shadowRoot.querySelector('.options')
-//     this.arrow = this.shadowRoot.querySelector('.arrow')
-//
-//     this.li = this.querySelectorAll('li')
-//     this.optionCount = this.li.length - 1
-//     // this.defaultValue = { textContent: this.getAttribute('placeholder'), className: 'placeholder' }
-//     // this.defaultValue = this.input.placeholder
-//   }
-//
-//   connectedCallback() {
-//     this.attachAttributes()
-//     this.attachEventHandlers()
-//     // this.updateValue()
-//   }
-//
-//   attachAttributes() {
-//     this.li.forEach((val, i) => val.setAttribute('tabindex', i))
-//   }
-//
-//   attachEventHandlers() {
-//     // this.input.addEventListener('focus', e => (e.target.focus(), e.target.select()))
-//     // this.input.addEventListener('click', e => e.target.select())
-//     this.autoComplete.addEventListener('keydown', this.keyDownHandler.bind(this))
-//     this.options.addEventListener('click', e => (this.selectValue(e.target), e.stopPropagation()))
-//     this.bar.addEventListener('click', e => {
-//       if (this.isOpen)  this.close()
-//       else {
-//         this.open()
-//         this.input.focus()
-//         this.input.select()
-//         e.stopPropagation()
-//       }
-//     })
-//     this.li.forEach((val, i) => val.addEventListener('mouseenter', e => (e.target.focus(), this.focusIndex = i)))
-//     // document.addEventListener('click', e => this.isOpen ? this.close() : void(0))
-//   }
-//
-//   updateValue(value) {
-//     console.log(value)
-//     this.input.value = value
-//   }
-//
-//   setFocus() {
-//     this.li[this.focusIndex].focus()
-//   }
-//
-//   selectValue(option) {
-//     this.li.forEach((val, i) => {
-//       if (val.hasAttribute('selected') && val != option) val.removeAttribute('selected')
-//       else if (val == option) {
-//         val.setAttribute('selected', 'selected')
-//         this.focusIndex = i
-//       }
-//     })
-//     this.updateValue(option.textContent)
-//     this.setFocus()
-//     this.fireChangeEvent()
-//     this.close()
-//   }
-//
-//   open() {
-//     this.isOpen = true
-//     this.arrow.setAttribute('transform', 'rotate(180, 5, 3)')
-//     this.dropdown.style.display = 'block'
-//     this.setFocus()
-//   }
-//
-//   close() {
-//     this.isOpen = false
-//     this.arrow.removeAttribute('transform')
-//     this.dropdown.style.display = 'none'
-//     this.bar.focus()
-//   }
-
-const makeRegularExpression = input => {
-
-  const escaped = []
-  const reserved = '.?*+!()[]/$^'
-  for (let i = 0; i < input.length; i++) {
-    escaped.push(reserved.includes(input[i]) ? '\\' + input[i] : input[i])
-  }
-  return new RegExp(escaped.join(''), 'i')
-}
-
 class AutoComplete extends HTMLElement {
   constructor() {
     super()
     this.attachShadow({ mode: 'open' })
     this.shadowRoot.appendChild(getTemplate('#autoCompleteTemplate').content.cloneNode(true))
-    this.focusIndex = 0
-    this.isOpen = false
-
-    this.selectBox = this.shadowRoot.querySelector('.box')
+    this.minChars = 1
+    this.delay = 150
+    this.offsetleft = 0
+    this.offsettop = 1
+    this.cache = {}
+    this.options = []
     this.input = this.shadowRoot.querySelector('input')
     this.dropdown = this.shadowRoot.querySelector('.dropdown')
-
-    this.x_icon = this.shadowRoot.querySelector('.x_icon')
-    this.arrow_icon = this.shadowRoot.querySelector('.arrow_icon  > path')
-
-    this.li = this.querySelectorAll('li')
-    // this.defaultValue = {
-    //   textContent: this.getAttribute('placeholder') || 'Select ...',
-    //   className: 'placeholder'
-    // }
   }
 
   connectedCallback() {
+    if (this.hasAttribute('placeholder')) this.input.placeholder = this.getAttribute('placeholder')
+    if (this.hasAttribute('name'))        this.input.name = this.getAttribute('name')
+    if (this.hasAttribute('min-chars'))   this.minChars = this.getAttribute('min-chars')
+    if (this.hasAttribute('offsetleft'))  this.offsetleft = this.getAttribute('offsetleft')
+    if (this.hasAttribute('offsettop'))   this.offsettop = this.getAttribute('offsettop')
+  }
+
+  init(data) {
+    this.options = data
     this.attachAttributes()
-    this.attachEventHandlers()
-    this.setUpSlotDefinedDefaults()
+    this.attachEventListeners()
   }
 
   attachAttributes() {
-    this.dropdown.hidden = true
-    this.selectBox.setAttribute('tabindex', -1)
-    this.li.forEach((val, i) => val.setAttribute('tabindex', i))
+    this.input.setAttribute('autocomplete', 'off')
+    this.input.setAttribute('spellcheck', false)
+    this.input.last_val = ''
   }
 
-  attachEventHandlers() {
-    this.addEventListener('click', this.toggle)
-    this.addEventListener('keyup', this.handleKeyPress)
-    this.dropdown.addEventListener('click', this.selectValue.bind(this))
-    this.x_icon.addEventListener('click', this.deselectValue.bind(this))
-    this.li.forEach(val => val.addEventListener('mouseenter', this.handleMouseEnter.bind(this)))
+  attachEventListeners() {
+    window.addEventListener('resize', this.updateSize.bind(this))
 
-    document.addEventListener('click', e => this.isOpen ? this.close() : void(0))
+    this.dropdown.addEventListener('mouseleave', this.mouseLeave.bind(this))
+    this.dropdown.addEventListener('mouseover',  this.mouseOver.bind(this))
+    this.dropdown.addEventListener('mousedown',  this.mouseDown.bind(this))
+
+    this.input.addEventListener('blur',    this.blur.bind(this))
+    this.input.addEventListener('keyup',   this.keyUp.bind(this))
+    this.input.addEventListener('keydown', this.keyDown.bind(this))
   }
 
-  setUpSlotDefinedDefaults() {
-    const hasDefaultSelection = this.querySelector('li[selected]')
-    if (!hasDefaultSelection) return /*this.updateValue(this.defaultValue)*/
-    this.focusIndex = hasDefaultSelection.tabIndex
-    this.selectValue()
+  dropdownDisplay(style) {
+    this.dropdown.style.display = style
   }
 
-  updateValue(textContent, className) {
-    this.x_icon.style.display = className == 'placeholder' ? 'none' : 'block'
-    this.input.setAttribute('placeholder', textContent)
-    console.log(this.input)
-    this.input.value = ''
-    this.focusIndex = 0
-    this.input.className = className
+  isDropdownHidden() {
+    return this.dropdown.style.display == 'none'
+  }
+  mouseLeave(e) {
+    if (!e.target.classList.contains('option')) return
+    const s = this.dropdown.querySelector('.option.selected')
+    if (s) setTimeout(_ => s.className = 'option', 20)
   }
 
-  selectValue(e) {
-    const currentSelected = this.querySelector('li[selected]')
-    if (currentSelected) currentSelected.removeAttribute('selected')
-    if (!e.target.hasAttribute('selected')) {
-      e.target.setAttribute('selected', '')
+  mouseOver(e) {
+    if (!e.target.classList.contains('option')) return
+    const s = this.dropdown.querySelector('.option.selected')
+    if (s) s.className = 'option'
+    e.target.className += ' selected'
+  }
+
+  mouseDown(e) {
+    if (!e.target.classList.contains('option')) return
+    const value = e.target.getAttribute('data-val')
+    this.input.value = value
+    this.onSelect(e, value, this)
+    this.dropdownDisplay('none')
+  }
+
+  blur(e) {
+    if (!document.querySelector('.list:hover')) {
+      this.input.last_val = this.input.value
+      this.dropdownDisplay('none')
+      setTimeout(_ => this.dropdownDisplay('none'), 350)
+    } else if (this.input !== document.activeElement) {
+      setTimeout(_e => this.input.focus(), 20)
     }
-    this.updateValue(e.target.textContent, 'selected')
-    // this.setFocus()
-    this.fireChangeEvent()
-    this.close()
-    if (e) e.stopPropagation()
   }
 
-  setFocus() {
-    const visibleOptions = this.getVisibleOptions()
-    if (visibleOptions.length) visibleOptions[this.focusIndex].focus()  }
+  keyUp(e) {
+    let key = e.which
+    if (!((key < KEYCODE.END || key > KEYCODE.DOWN)
+        && key != KEYCODE.ENTER
+        && key != KEYCODE.ESCAPE)) return
 
-  open() {
-    this.isOpen = true
-    this.arrow_icon.setAttribute('transform', 'rotate(180, 5, 3)')
-    this.dropdown.hidden = false
-    this.input.focus()
-    this.input.select()
-    // this.setFocus()
+    const value = this.input.value
+
+    if (!(value.length >= this.minChars)) {
+      this.input.last_val = value
+      this.dropdownDisplay('none')
+    }
+
+    if (value == this.input.last_val) return
+
+    this.input.last_val = value
+    clearTimeout(this.input.timer)
+
+    if (!this.cache) return
+    if (value in this.cache)
+      return this.suggest(this.cache[value])
+
+    for (let i = 1; i < value.length - this.minChars; i++) {
+      const part = value.slice(0, value.length - i)
+      if (part in this.cache && !this.cache[part].length)
+        return this.suggest([])
+    }
+
+    this.input.timer = setTimeout(_ => this.source(value, this.suggest), this.delay)
   }
 
-  close() {
-    this.isOpen = false
-    this.arrow_icon.removeAttribute('transform')
-    this.dropdown.hidden = true
-    this.selectBox.focus()
-  }
-
-  toggle(e) {
-    this.isOpen ? this.close() : this.open()
-    e.stopPropagation()
-  }
-
-  getVisibleOptions() {
-    return this.querySelectorAll('li:not([hidden]')
-  }
-
-  handleKeyPress(e) {
+  keyDown(e) {
+    const prev = this.dropdown.querySelector('.selected')
+    let next
     switch(e.which) {
 
-      // case KEYCODE.BACKSPACE:
-      //   this.deselectValue(e)
-      //   break
-
-      case KEYCODE.ESCAPE:
-        this.close()
+      case KEYCODE.DOWN:
+        if (this.isDropdownHidden()) return this.dropdownDisplay('block')
+        if (prev) {
+          prev.className = 'option'
+          next = prev.nextSibling
+        } else {
+          next = this.dropdown.querySelector('.option')
+        }
+        next ? next.className += ' selected' : next = 0
+        this.input.value = next ? next.getAttribute('data-val') : this.input.last_val
+        this.updateSize(0, next)
         break
 
       case KEYCODE.UP:
-        const length = this.getVisibleOptions().length
-        this.focusIndex = (this.focusIndex - 1 + length) % length
-        this.setFocus()
+        if (this.isDropdownHidden()) return this.dropdownDisplay('block')
+        if (prev) {
+          prev.className = 'option'
+          next = prev.previousSibling
+        } else {
+          next = this.dropdown.childNodes[this.dropdown.childNodes.length - 1]
+        }
+        next ? next.className += ' selected' : next = 0
+        this.input.value = next ? next.getAttribute('data-val') : this.input.last_val
+        this.updateSize(0, next)
         break
 
-      case KEYCODE.DOWN:
-        if (!this.isOpen) return this.open()
-        this.focusIndex = (this.focusIndex + 1) % this.getVisibleOptions().length
-        this.setFocus()
+      case KEYCODE.ESCAPE:
+        this.input.value = this.input.last_val
+        this.dropdownDisplay('none')
         break
 
       case KEYCODE.ENTER:
-        if (this.isOpen) this.selectValue(e)
-        else this.open()
+        const s = this.dropdown.querySelector('.option.selected')
+        if (s && !this.isDropdownHidden()) {
+          this.onSelect(e, s.getAttribute('data-val'), s)
+          setTimeout(_ => this.dropdownDisplay('none'), 20)
+        }
         break
 
       default:
-        this.input.focus()
-        this.filter(this.input.value)
         break
     }
   }
 
-  filter(value) {
-    if (escape(value).includes('%5C')) return
-    const regEx = makeRegularExpression(value)
-    this.li.forEach(val => {
-      val.hidden = !regEx.test(val.textContent)
-      console.log(val.textContent, regEx.test(val.textContent))
-    })
+  focus(e) {
+    this.input.last_val = '\n'
+    this.keyUp(e)
   }
 
-  deselectValue(e) {
-    const selected = this.querySelector('li[selected]')
-    if (!selected) return e.stopPropagation()
-    selected.removeAttribute('selected')
-    this.updateValue('', 'placeholder')
-    this.focusIndex = 0
-    this.setFocus()
-    this.open()
-    this.fireChangeEvent()
-    e.stopPropagation()
+  source(term, suggest) {
+    term = term.toLowerCase()
+    const matched = this.options.filter(val => ~val.toLowerCase().indexOf(term))
+    this.suggest(matched)
   }
 
-  handleMouseEnter(i, e) {
-    e.target.focus()
-    let i = 0
-    const visibleOptions = this.getVisibleOptions()
-    while (visibleOptions[i] != e.target) ++i
-    this.focusIndex = i
+  onSelect(e, term, item) {}
+
+  suggest(data) {
+    const val = this.input.value
+    this.cache[val] = data
+    if (data.length && val.length >= this.minChars) {
+      let s = ''
+      for (let i = 0; i < data.length; i++) s += this.renderOptions(data[i], val)
+      this.dropdown.innerHTML = s
+      return this.updateSize(0)
+    }
+    this.dropdownDisplay('none')
   }
 
-  fireChangeEvent() {
-    const selected = this.querySelector('li[selected]')
-    const detail = selected
-      ? { value: selected.value, textContent: selected.textContent }
-      : { value: '', textContent: '' }
-    this.dispatchEvent(new CustomEvent('change', { detail }))
+  renderOptions(item, search) {
+    search = search.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&')
+    const re = new RegExp(`(${search.split(' ').join('|')})`, 'gi')
+    return `<div class="option" data-val="${item}">${item.replace(re, '<b>$1</b>')}</div>`
   }
 
-  addOneLi(val) {
-    this.createLiElement(val)
-    this.li = this.querySelectorAll('li')
-  }
+  updateSize(resize, next) {
+    const rect = this.input.getBoundingClientRect()
+    this.dropdown.style.left = (rect.left + window.pageXOffset + this.offsetleft) | 0 + 'px'
+    this.dropdown.style.top = (rect.bottom + window.pageYOffset + this.offsettop) | 0 + 'px'
+    this.dropdown.style.width = (rect.right - rect.left) | 0 + 'px'
 
-  addManyLi(array) {
-    array.forEach((val, i) => this.createLiElement(val, this.li.length + i))
-    this.li = this.querySelectorAll('li')
-  }
+    if (resize) return
 
-  createLiElement(val, i = 0) {
-    const li = document.createElement('li')
-    for (let prop in val) li[prop] = val[prop]
-    li.setAttribute('tabindex', i)
-    li.addEventListener('mouseenter', this.handleMouseEnter.bind(this, i))
-    this.appendChild(li)
+    this.dropdownDisplay('block')
+
+    if (!this.dropdown.maxHeight)
+      this.dropdown.maxHeight = parseInt(getComputedStyle(this.dropdown, null).maxHeight)
+
+    const auto = this.dropdown.querySelector('.option')
+
+    if (!this.dropdown.height && auto) this.dropdown.height = auto.offsetHeight
+
+    if (!next) return this.dropdownrollTop = 0
+
+    const scrTop = this.dropdown.scrollTop
+    const selTop = next.getBoundingClientRect().top - this.dropdown.getBoundingClientRect().top
+    const greaterThanZero = selTop + this.dropdown.height - this.dropdown.maxHeight > 0
+
+    if (greaterThanZero)
+      this.dropdown.scrollTop = selTop + this.dropdown.height + scrTop - this.dropdown.maxHeight
+    else if (selTop < 0)
+      this.dropdown.scrollTop = selTop + scrTop
   }
 
   disconnectedCallback() {
-    this.removeEventListener('keydown', this.handleKeyPress)
-    this.removeEventListener('click', this.toggle)
-    this.x_icon.removeEventListener('click', this.deselectValue)
-    this.dropdown.removeEventListener('click', this.selectValue)
-    this.li.forEach(val => (val.remove(), val = null))
+    this.dropdown.removeEventListener('mouseleave', this.mouseLeave)
+    this.dropdown.removeEventListener('mouseover',  this.mouseOver)
+    this.dropdown.removeEventListener('mousedown',  this.mouseDown)
+    this.input.removeEventListener('resize',  this.updateSize)
+    this.input.removeEventListener('keydown', this.keyDown)
+    this.input.removeEventListener('keyup',   this.keyUp)
+    this.input.removeEventListener('blur',    blur)
+    this.input.removeEventListener('focus',   focus)
+    let autocomplete = getTemplate('#autoCompleteTemplate')
+    autocomplete = null
   }
+
 }
 
 customElements.define('x-auto-complete', AutoComplete)
+
+
+

9dc709c58fec1e9869fa64e330e8372a75af05d4
Patrice Sandhu <psandhu@zoiclabs.com>
2019-06-22 19:30:33 -0700

    add button example

---
+++ Button/Button.js
@@ -0,0 +1,44 @@
+class Button extends HTMLElement {
+  constructor() {
+    super()
+
+    this.attachShadow({ mode: 'open' })
+    this.shadowRoot.appendChild(getTemplate('#buttonTemplate').content.cloneNode(true))
+    this.button = this.shadowRoot.querySelector('button')
+    this.container = this.shadowRoot.querySelector('.container')
+
+    this.button.addEventListener('click', this.onClick)
+  }
+
+  get label() {
+    return this.getAttribute('label')
+  }
+
+  set label(value) {
+    this.setAttribute('label', value)
+  }
+
+  static get observedAttributes() {
+    return ['label']
+  }
+
+  attributeChangedCallback(name, oldVal, newVal) {
+    this.render()
+  }
+
+  connectedCallback() {
+    if (this.hasAttribute('as-atom')) {
+      this.container.style.padding = '0'
+    }
+  }
+
+  onClick = e => {
+    this.dispatchEvent(new CustomEvent('onClick', { detail: 'Button Clicked!'}))
+  }
+
+  render() {
+    this.button.textContent = this.label
+  }
+}
+
+window.customElements.define('my-button', Button)
